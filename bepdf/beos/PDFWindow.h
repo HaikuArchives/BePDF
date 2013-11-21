HaikuArchives/BePDF

commit db91d75c1ece71e4c42b9a6dffcb83c2624cec58

Rearrange message listing.

bepdf/beos/PDFWindow.h
@@ -87,19 +87,24 @@ public:
 		TYPE3_FONT_RENDERER_CMD,
 		PREFERENCES_FILE_CMD,
 		FILE_INFO_CMD,
-		// Printing
-		PAGESETUP_FILE_CMD,
-		PRINT_SETTINGS_CMD,
-//		PRINT_FILE_CMD,
+			// Printing
+			PAGESETUP_FILE_CMD,
+			PRINT_SETTINGS_CMD,
+			// PRINT_FILE_CMD,
+
 		// Edit
 		COPY_SELECTION_CMD,
 		SELECT_ALL_CMD,
 		SELECT_NONE_CMD,
 		COPY_LINK_CMD,
-		// User defined bookmarks
-		ADD_BOOKMARK_CMD,
-		DELETE_BOOKMARK_CMD,
-		EDIT_BOOKMARK_CMD,
+		
+		// Zoom
+		SET_ZOOM_VALUE_CMD,
+		SET_CUSTOM_ZOOM_FACTOR_CMD,
+		ZOOM_IN_CMD,
+		ZOOM_OUT_CMD,
+		FIT_TO_PAGE_WIDTH_CMD,
+		FIT_TO_PAGE_CMD,
 		
 		// Annotations
 		FIRST_ANNOT_CMD,
@@ -143,13 +148,7 @@ public:
 		HISTORY_BACK_CMD,
 		HISTORY_FORWARD_CMD,
 		PAGE_SELECTED_CMD,
-		// Zoom
-		SET_ZOOM_VALUE_CMD,
-		SET_CUSTOM_ZOOM_FACTOR_CMD,
-		ZOOM_IN_CMD,
-		ZOOM_OUT_CMD,
-		FIT_TO_PAGE_WIDTH_CMD,
-		FIT_TO_PAGE_CMD,
+
 		// Rotation
 		SET_ROTATE_VALUE_CMD,
 		ROTATE_CLOCKWISE_CMD,
@@ -162,6 +161,7 @@ public:
 		// Search
 		FIND_CMD,
 		FIND_NEXT_CMD,
+
 		// Help
 		HELP_CMD,
 		ONLINE_HELP_CMD,

commit 09a2001006df26882de21639c483770baf076e7c

Fix bug introduced in db91d75c1ece71e4c42.

bepdf/beos/PDFWindow.h
@@ -161,6 +161,11 @@ public:
 		// Search
 		FIND_CMD,
 		FIND_NEXT_CMD,
+		
+		// User defined bookmarks
+		ADD_BOOKMARK_CMD,
+		DELETE_BOOKMARK_CMD,
+		EDIT_BOOKMARK_CMD,
 
 		// Help
 		HELP_CMD,